79a5a4f9ecab83dce822198e3c1629fde4562041
Junior Bueno <juniorbueno@email.com>
2025-06-17 10:05:00 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'projeto-poke-api',
+  webDir: 'www'
+};
+
+export default config;
